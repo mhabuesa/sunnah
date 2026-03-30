706d2e3f50bfc5d22bb9bedf8a35c8d5321a474b
mhabuesa <abuesa1279@gmail.com>
2026-03-31 01:17:40 +0600

    App Service Provider Update

--- database/migrations/2026_03_22_082827_create_orders_table.php
+++ database/migrations/2026_03_22_082827_create_orders_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->string('invoice_no', 20)->unique();
             $table->foreignId('customer_id')->constrained()->cascadeOnDelete();
             $table->string('seller_id')->nullable();
             $table->string('order_type')->nullable(); // pos / web
