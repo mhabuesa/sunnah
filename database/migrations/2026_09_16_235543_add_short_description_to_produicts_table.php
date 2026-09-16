54727f2f112ac70f253e99426c6efa3751626ef2
mhabuesa <abuesa1279@gmail.com>
2026-09-17 00:03:56 +0600

    TextEditor Edit

--- database/migrations/2026_09_16_235543_add_short_description_to_produicts_table.php
+++ database/migrations/2026_09_16_235543_add_short_description_to_produicts_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('products', function (Blueprint $table) {
-             $table->string('shortDescription')->nullable()
+             $table->longText('shortDescription')->nullable()
                   ->after('name');
         });
     }
